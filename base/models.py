bd198379adf585ad4265907c95be7e2178bd8fe6
yassine <yassine.jrad@esprit.tn>
2020-10-18 01:45:42 +0100

    WIP and DB fix

--- base/models.py
+++ base/models.py
@@ -1,16 +1,16 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-#
-#
-# class ContactForm(models.Model):
-#     name = models.CharField(max_length=200, help_text="Name of the sender")
-#     email = models.EmailField(max_length=200)
-#     subject = models.CharField(max_length=200)
-#     message = models.TextField(max_length=500)
-#     date = models.DateTimeField(auto_now_add=True)
-#
-#     class Meta:
-#         verbose_name_plural = "Feedback"
-#
-#     def __str__(self):
-#         return self.name + "-" + self.email
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=200, help_text="Name of the sender", null=True)
+    email = models.EmailField(max_length=200, null=True)
+    subject = models.CharField(max_length=200, null=True)
+    message = models.TextField(max_length=500, null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Feedback"
+
+    def __str__(self):
+        return self.name + "-" + self.email
--- base/views.py
+++ base/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# from .models import ContactForm
+from .models import ContactForm
 
 
 # Create your views here.
 def home(request):
-    # name = request.GET.get('name')
-    # email = request.GET.get('email')
-    # subject = request.GET.get('subject')
-    # message = request.GET.get('message')
-    #
-    # data = ContactForm(name=name, email=email, subject=subject, message=message)
-    # data.save()
+    name = request.GET.get('name')
+    email = request.GET.get('email')
+    subject = request.GET.get('subject')
+    message = request.GET.get('message')
+
+    data = ContactForm(name=name, email=email, subject=subject, message=message)
+    data.save()
     return render(request, 'base/index.html', {})
 
