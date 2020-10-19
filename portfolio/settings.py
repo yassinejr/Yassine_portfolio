93d89bea41cd21faf6860df4b26f6c9b53ddfea9
yassine <yassine.jrad@esprit.tn>
2020-10-19 22:22:51 +0100

    wip

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATIC_FILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
